7264f9757de6b5ebad1a62b1097403fac97f1cd4
DuyKhai129 <nguyenduykhai132@gmail.com>
2023-08-03 22:51:50 +0700

    use mysql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express"); // commonjs
 require("dotenv").config();
+
 const configViewEngine = require("./config/viewEngine");
 //import express from "express" es modules
 const app = express();
@@ -7,9 +8,18 @@ const port = process.env.PORT || 8080;
 const hostName = process.env.HOST_NAME;
 // router
 const webRouter = require("./routes/web");
-
+// import connection ctrl  + click
+const connection = require("./config/database");
 //config template engine
 configViewEngine(app);
+
+// get connection
+
+// simple query
+connection.query("SELECT * from Users u", function (err, results, fields) {
+  console.log(">>>results: ", results); // results contains rows returned by server
+  // console.log(">>>fields: ", fields); // fields contains extra meta data about results, if available
+});
 
 // route
 app.use("/views", webRouter);
